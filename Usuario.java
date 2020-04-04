b45e4f3b2671396454bcb0d58d5eb43fc3eec1c0
Camila Burgueño <camilav28@hotmail.com>
2020-04-03 21:35:40 -0300

    agregue getters and setters

--- Usuario.java
+++ Usuario.java
@@ -6,6 +6,54 @@ public class Usuario{
    int edad;
    int cantidadDeHijos;
    int saldo=0;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCédula() {
+        return cédula;
+    }
+
+    public void setCédula(String cédula) {
+        this.cédula = cédula;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCantidadDeHijos() {
+        return cantidadDeHijos;
+    }
+
+    public void setCantidadDeHijos(int cantidadDeHijos) {
+        this.cantidadDeHijos = cantidadDeHijos;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
     
     
 }
